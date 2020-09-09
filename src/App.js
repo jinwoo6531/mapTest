988551e43431a043da8252bc2a4dabdd8bb27bde
jinwoo <jintakk6531@gmail.com>
2020-09-09 16:39:28 +0900

    9/9 config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { RenderAfterNavermapsLoaded } from "react-naver-maps";
 import Lunch from "./components/lunch/lunch";
+import { clientKey } from "./config";
 
 function App() {
   return (
     <RenderAfterNavermapsLoaded
-      ncpClientId={"h0nir5z22z"} // 자신의 네이버 계정에서 발급받은 Client ID
+      ncpClientId={clientKey} // 자신의 네이버 계정에서 발급받은 Client ID
       error={<p>Maps Load Error</p>}
       loading={<p>Maps Loading...</p>}
     >
