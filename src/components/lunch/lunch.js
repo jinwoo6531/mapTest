4fc9a1e7920aef0dad6d9eb87a3e9235d95bc54f
jinwoo <jintakk6531@gmail.com>
2020-11-17 14:39:12 +0900

    11/17 수정

--- src/components/lunch/lunch.js
+++ src/components/lunch/lunch.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { NaverMap, Marker } from "react-naver-maps";
-import "./style.css";
-import markerPng from "./marker_v2.png";
-import markerPng2 from "./marker_v1.png";
-import { data } from "../data/data";
+import React from 'react';
+import { NaverMap, Marker } from 'react-naver-maps';
+import './style.css';
+import markerPng from './marker_v2.png';
+import markerPng2 from './marker_v1.png';
+import { data } from '../data/data';
 
 function lunch() {
   const navermaps = window.naver.maps;
 
   return (
     <>
-      <div id="navbar">집ㅎㅎ</div>
+      <div id="navbar">네이버 Map API를 활용한 nodejs</div>
       <div id="infoBox">
         <div id="infoTitle">현재 날짜</div>
         <div id="infoContent">2020.09.10</div>
       </div>
       <NaverMap
-        mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
+        mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
         style={{
-          width: "100%", // 네이버지도 가로 길이
-          height: "100vh", // 네이버지도 세로 길이
+          width: '100%', // 네이버지도 가로 길이
+          height: '100vh', // 네이버지도 세로 길이
         }}
         defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
         defaultZoom={11} // 지도 초기 확대 배율
       >
         {data.map((item) => {
-          if (item.content === "비트교육센터") {
+          if (item.content === '비트교육센터') {
             return (
               <Marker
                 key={1}
@@ -38,14 +38,14 @@ function lunch() {
                 }}
                 shape={{
                   coords: [0, 12, 12, 0, 24, 12, 12, 32, 0, 12],
-                  type: "poly",
+                  type: 'poly',
                 }}
                 onClick={() => {
                   alert(`여기는 ${item.content}입니다.`);
                 }}
               />
             );
-          } else if (item.content === "지영이집") {
+          } else if (item.content === '지영이집') {
             return (
               <Marker
                 key={1}
@@ -58,12 +58,12 @@ function lunch() {
                 }}
                 shape={{
                   coords: [0, 12, 12, 0, 24, 12, 12, 32, 0, 12],
-                  type: "poly",
+                  type: 'poly',
                 }}
-                onClick={() => alert("지영이네집")}
+                onClick={() => alert('지영이네집')}
               />
             );
-          } else if (item.content === "바울이집") {
+          } else if (item.content === '바울이집') {
             return (
               <Marker
                 key={1}
@@ -76,12 +76,12 @@ function lunch() {
                 }}
                 shape={{
                   coords: [0, 12, 12, 0, 24, 12, 12, 32, 0, 12],
-                  type: "poly",
+                  type: 'poly',
                 }}
-                onClick={() => alert("바울이네집")}
+                onClick={() => alert('바울이네집')}
               />
             );
-          } else if (item.content === "재영이집") {
+          } else if (item.content === '재영이집') {
             return (
               <Marker
                 key={1}
@@ -94,9 +94,9 @@ function lunch() {
                 }}
                 shape={{
                   coords: [0, 12, 12, 0, 24, 12, 12, 32, 0, 12],
-                  type: "poly",
+                  type: 'poly',
                 }}
-                onClick={() => alert("재영이네집")}
+                onClick={() => alert('재영이네집')}
               />
             );
           }
